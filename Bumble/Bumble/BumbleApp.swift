4f85cf1abecca1cfa9784588f4bb1a937a9573c5
Alysson Menezes Dodo <almenezes912@gmail.com>
2025-07-22 18:25:57 -0300

    start project

---
+++ Bumble/Bumble/BumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumbleApp.swift
+//  Bumble
+//
+//  Created by ALYSSON MENEZES on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
